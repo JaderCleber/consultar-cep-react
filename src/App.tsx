566b406cdc2db3725b293332ceed444ff246b222
jadercleber <jader.cleber@gmail.com>
2019-06-06 09:35:14 -0300

    Search and map complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,127 @@
-import React, { useState } from 'react';
+import React from 'react';
 import MapCard from './components/MapCard';
+import { Grid, Paper, InputLabel, FormControl, Input, Typography, Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import Geocode from 'react-geocode';
+import classnames from 'classnames';
 
-class App extends React.PureComponent {
+Geocode.setApiKey("AIzaSyAExktO4hzgGp4gbIMlOoQgtBAMTU6XyiM");
+
+const styles = (theme: any) => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    margin: '2%'
+  },
+  gray: {
+    background: '#efefef'
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+});
+
+interface IState {
+  cep: string;
+  found: {
+    cep?: string;
+    street?: string;
+    neighborhood?: string;
+    city?: string;
+    startNumber?: string;
+  };
+  location: any;
+}
+
+interface PropTypes {
+  classes: any;
+}
+
+class App extends React.PureComponent<PropTypes, IState> {
   state = {
     cep: '',
-    found: null,
+    found: {},
+    location: null
+  };
+
+  constructor(props: any) {
+    super(props);
   }
 
-  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => this.setState({ cep: e.target.value });
-  handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    this.setState({ found: false }, () => {
-      // FETCH GOOGLE API
-      fetch(`http://maps.google.com/maps/api/geocode/json?address=${this.state.cep}&sensor=false`, () => {
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    this.setState({ cep: e.target.value.replace(/\D/g, '') });
 
-      })
+  handleClick = () => {
+    this.setState({ found: {}, location: null }, async () => {
+      const response = await fetch(`https://viacep.com.br/ws/${this.state.cep}/json/`);
+      if (response.status === 200) {
+        const data = await response.json();
+        const geolocation = await Geocode.fromAddress(data.cep);
+        const { location } = geolocation.results[0].geometry;
+        this.setState({
+          found: {
+            cep: data.cep,
+            street: data.logradouro,
+            neighborhood: data.bairro,
+            city: `${data.localidade} - ${data.uf}`,
+            startNumber: data.complemento.substr(3, data.complemento.indexOf('/'))
+          },
+          location
+        });
+      }
+
     });
   };
 
-  render() {
-    const { cep, found } = this.state;
-    return (
-      <div className="App">
-        <h3>Consultar</h3>
-        <h3>CPE</h3>
-        <input value={cep} onChange={this.handleChange} />
-        <button onClick={this.handleClick} />
+  handleClose = () => this.setState({ found: {}, cep: '' });
 
-        {found && (
-          <MapCard cep={cep} />
+  render() {
+    const { cep, found, location }: IState = this.state;
+    const { classes } = this.props;
+    return (
+      <Grid container className={classes.root} spacing={10}>
+        <Grid item xs={12}>
+          <Paper className={classnames(classes.paper, classes.gray)}>
+            <Typography gutterBottom variant="h6">
+              Consultar
+						</Typography>
+            <Grid container spacing={10}>
+              <Grid item sm={2}>
+                <Typography gutterBottom variant="overline">
+                  CEP
+						</Typography>
+              </Grid>
+              <Grid item xs={2}>
+                <FormControl className={classes.margin}>
+                  <Input id="cep-search" value={cep} onChange={this.handleChange} />
+                </FormControl>
+              </Grid>
+              <Grid item xs={2}>
+                <Button onClick={this.handleClick} variant="contained" color="primary" className={classes.button}>
+                  Primary
+      </Button>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        {found.cep && (
+          <Grid item xs={12}>
+            <MapCard
+              cep={found.cep}
+              street={found.street}
+              neighborhood={found.neighborhood}
+              city={found.city}
+              onClose={this.handleClose}
+              classes={classes}
+              location={location}
+            />
+          </Grid>
         )}
-      </div>
+      </Grid>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/MapCard/index.tsx
+++ src/components/MapCard/index.tsx
@@ -1,31 +1,84 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Grid, Paper, Button, Typography } from '@material-ui/core';
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 interface IMapCard {
-  street: string;
-  neighborhood: string;
-  city: string;
-  cep: string;
-  onClose(): void;
-  google: any;
+	street: string | undefined;
+	neighborhood: string | undefined;
+	city: string | undefined;
+	cep: string | undefined;
+	onClose(): void | undefined;
+	classes: any;
+	google: any;
+	location: any;
 }
 
-const MapCard: React.FC<IMapCard> = ({ google, street, neighborhood, city, cep, onClose }) => {
-  return (
-    <div>
-      <button onClick={onClose}>X</button>
-      <h1>{street}</h1>
-      <p id="neighborhood">{neighborhood}</p>
-      <p id="city">{city}</p>
-      <p id="cep">{cep}</p>
-      <Map google={google} zoom={14}>
-        <Marker name={'CEP consultado'} />
-      </Map>
-    </div>
-  );
-}
+const MapCard: React.FC<IMapCard> = ({
+	google,
+	street,
+	neighborhood,
+	city,
+	cep,
+	onClose,
+	classes,
+	location
+}) => {
+	const style = new google.maps.StyledMapType([{
+		stylers: [{
+			weight: 100,
+		}]
+	}]);
+	// new Promise(async (resolve, reject) => {
+	// 	const geocoder = new google.maps.Geocoder();
+	// 	const map = new google.maps.Map(document.getElementById('map'), {
+	// 		zoom: 14,
+	// 	});
+	// 	geocoder.geocode({ 'address': cep }, (results: google.maps.GeocoderResult[], status: any) => {
+	// 		if (status === 'OK') {
+	// 			map.setCenter(results[0].geometry.location);
+	// 			const marker = new google.maps.Marker({
+	// 				map: map,
+	// 				position: results[0].geometry.location
+	// 			});
+	// 		}
+	// 		resolve();
+	// 	});
+	// });
+
+	return (
+		<Paper className={classes.paper}>
+			<Grid container spacing={10} justify="flex-end">
+				<Grid item xs={2}>
+					<Button onClick={onClose}>X</Button>
+				</Grid>
+			</Grid>
+
+			<Typography gutterBottom variant="h5">
+				{street}
+			</Typography>
+			<Typography gutterBottom variant="body1" id="neighborhood">
+				{neighborhood}
+			</Typography>
+			<Typography gutterBottom variant="body1" id="city">
+				{city}
+			</Typography>
+			<Typography gutterBottom variant="body1" id="cep">
+				{cep}
+			</Typography>
+
+			<Grid container spacing={10} justify="center">
+				<Grid item xs={4}>
+					<Map google={google} zoom={14} initialCenter={location} styles={style}>
+						<Marker position={location} />
+					</Map>
+				</Grid>
+			</Grid>
+			{/* <div id="map"></div> */}
+
+		</Paper>
+	);
+};
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCdl6zLIxYxbis_CqbtFl3NjYOmOedy38c',
+	apiKey: 'AIzaSyAExktO4hzgGp4gbIMlOoQgtBAMTU6XyiM',
 })(MapCard)
